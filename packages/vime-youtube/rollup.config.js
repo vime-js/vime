aa3a76b493f90a49e822d262dbad775ab732f25e
Rahim Alwer <rahim_alwer@hotmail.com>
2020-03-01 16:00:21 +1100

    chore: simplify rollup configs

--- packages/vime-youtube/rollup.config.js
+++ packages/vime-youtube/rollup.config.js
@@ -1,21 +1,5 @@
-import {
-  chunkedBuild, modernBuild, legacyBuild, getFileName
-} from '../../rollup-base';
+import { providerBuild } from '../../rollup-base';
 
-const entry = 'src/index.js';
 const name = 'YouTube';
-const lite = { input: 'src/YouTubeLite.svelte' };
-const full = { input: 'src/YouTube.js' };
 
-const chunks = id => {
-  if (id.includes('vime-youtube/src')) getFileName(id);
-};
-
-export default [
-  chunkedBuild({ input: { youtube: entry }, chunks }),
-  chunkedBuild({ input: { youtube: entry }, format: 'system', chunks }),
-  legacyBuild({ ...lite, name }),
-  legacyBuild({ ...full, name }),
-  modernBuild({ ...lite, fileName: 'FileSizeLite' }),
-  modernBuild({ ...full, fileName: 'FileSizeFull' })
-];
+export default providerBuild({ name });
