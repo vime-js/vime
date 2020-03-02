145cf1c5083149b9d7db14971c5ea0743150bd1f
Rahim Alwer <rahim_alwer@hotmail.com>
2020-03-02 23:04:37 +1100

    chore(player): migrate to monorepo wip

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,7 @@ module.exports = {
     '../packages/vime-youtube/stories/*stories.js',
     '../packages/vime-dailymotion/stories/*stories.js',
     '../packages/vime-vimeo/stories/*stories.js',
-    '../packages/vime-html5/stories/*stories.js'
+    '../packages/vime-html5/stories/*stories.js',
+    '../packages/vime-player/stories/*stories.js'
   ]
 };
