ba706ed0dce84562eb7c66a172ebadb2f6057e0d
Rahim Alwer <rahim_alwer@hotmail.com>
2020-03-12 15:55:47 +1100

    chore: remove old tests

--- packages/vime-player/src/plugins/__tests__/Scrim.test.js
+++
@@ -1,9 +0,0 @@
-import Scrim from '../Scrim';
-
-describe('components', () => {
-  describe('Scrim', () => {
-    it('should look structurally correct', () => {
-      expect(Scrim).toMatchRenderedSnapshot({ isActive: true });
-    });
-  });
-});
