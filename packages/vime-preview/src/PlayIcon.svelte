f2ce0a3507e4263a7883b02a94fd5e97c1b3f7b6
Rahim Alwer <rahim_alwer@hotmail.com>
2020-03-22 15:19:38 +1100

    feat(preview): inline play icon to avoid additional setup

---
+++ packages/vime-preview/src/PlayIcon.svelte
@@ -0,0 +1,21 @@
+<svg xmlns="http://www.w3.org/2000/svg">
+  <path d="M3 1.52c0-.398.644-.637.977-.418L15 8.662a.5.5 0 010 .835L3.977 16.911c-.333.219-.977-.02-.977-.418V1.52z"/>
+</svg>
+
+<style>
+  svg {
+    display: block;
+    fill: currentColor;
+    pointer-events: none;
+    width: 18px;
+    height: 18px;
+    color: #fff;
+    transform: scale(2);
+  }
+
+  @media (min-width: 680px) {
+    svg {
+      transform: scale(3.5);
+    }
+  }
+</style>
