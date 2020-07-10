d0a838df7606e478d8e5217a2451118c2cdb4d81
Rahim Alwer <rahim_alwer@hotmail.com>
2020-07-10 10:57:32 +1000

    chore: lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,29 +4,30 @@ module.exports = {
     browser: true,
     es6: true,
     jest: true,
-    node: true
+    node: true,
   },
   globals: {
     // Jest (jest-svelte-events)
-    listen: 'readonly'
+    listen: 'readonly',
   },
   extends: ['eslint-config-airbnb-base'],
   plugins: ['svelte3'],
   overrides: [{
-      files: ['**/*.svelte'],
-      processor: 'svelte3/svelte3'
+    files: ['**/*.svelte'],
+    processor: 'svelte3/svelte3',
   }],
   parserOptions: {
     ecmaVersion: 9,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   settings: {
     'svelte3/ignore-styles': (attrs) => attrs.type === 'text/scss',
   },
   rules: {
-    'no-unused-expressions': [2, { 'allowTernary': true }],
+    'no-unused-vars': 'off',
+    'no-unused-expressions': [2, { allowTernary: true }],
     // Utils use snakecase.
-    'camelcase': 'off',
+    camelcase: 'off',
     // Doesn't work with Svelte.
     'no-multiple-empty-lines': 'off',
     // These do not currently work with eslint-plugin-svelte3.
@@ -36,6 +37,8 @@ module.exports = {
     'import/no-mutable-exports': 'off',
     'import/no-extraneous-dependencies': 'off',
     'import/prefer-default-export': 'off',
-    'import/no-unresolved': 'off',	'require-atomic-updates': 'off',
-  }
-}
+    'import/no-unresolved': 'off',
+    'require-atomic-updates': 'off',
+  },
+};
+
