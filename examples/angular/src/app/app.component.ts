da1849c4f0f06d4dca1f178ec464585370a47881
Rahim Alwer <rahim_alwer@hotmail.com>
2020-08-14 17:57:47 +1000

    chore: dont lint examples dir

--- examples/angular/src/app/app.component.ts
+++ examples/angular/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = 'angular-example';
+  title = 'angular-example'
 
   onPlaying(event: CustomEvent<boolean>) {
     console.log('playing:', event.detail);
