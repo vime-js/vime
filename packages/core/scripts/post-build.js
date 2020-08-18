b94310baa437cd9925a9e88ada7aafac4a28b5b7
Rahim Alwer <rahim_alwer@hotmail.com>
2020-08-18 12:16:16 +1000

    docs: rewrite api doc urls and escape pipes in tables

--- packages/core/scripts/post-build.js
+++ packages/core/scripts/post-build.js
@@ -1,18 +1,35 @@
-const { getComponentsDocs, toTitleCase } = require('../../docs/helpers/components');
 const { readFileSync, writeFileSync } = require('fs');
+const { formatSideBarLabel, getComponentsDocs } = require('../../docs/helpers/components');
 
-const writeFrontMatters = () => {
-  for (const doc of getComponentsDocs()) {
-    let content = readFileSync(doc).toString();
-    const name = /^#\s([a-z-]+)/.exec(content)[1];
-    content = content.replace(/^#\s[a-z-]+/,
-      '---\n'
-      + `title: ${name}\n`
-      + `sidebar_label: ${toTitleCase(name.replace('vime', ''))}\n`
-      + '---'
-    );
-    writeFileSync(doc, content);
-  }
+const writeFrontMatter = (content) => {
+  const name = /^#\s([a-z-]+)/.exec(content)[1];
+  const label = formatSideBarLabel(name);
+  return content.replace(/^#\s[a-z-]+/,
+    '---\n'
+    + `title: ${name}\n`
+    + `sidebar_label: ${label}\n`
+    + `slug: api\n`
+    + '---'
+  );
 };
 
-writeFrontMatters();
+const rewriteURLs = (content) => {
+  return content.replace(/\[[a-z-]+\]\(.+\)/g, (match) => {
+    if (match === null) return;
+    const componentName = /\[(.+)\]/.exec(match)[1];
+    const path = /\((.+)\)/.exec(match)[1];
+    return `[${componentName}](${path}/readme.md)`;
+  });
+}
+
+const escapePipeInTables = (content) => {
+  return content.replace(/\s\\\|\s/g, ' ∣ ');
+};
+
+for (const doc of getComponentsDocs()) {
+  let content = readFileSync(doc).toString();
+  content = writeFrontMatter(content);
+  content = rewriteURLs(content);
+  content = escapePipeInTables(content);
+  writeFileSync(doc, content);
+}
