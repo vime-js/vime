1bc81f01cf6b83dca4db4dfe59fe2144dbb95521
Rahim Alwer <rahim_alwer@hotmail.com>
2020-08-19 11:30:06 +1000

    docs: fix broken links

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -7,7 +7,6 @@ module.exports = {
       label: 'Welcome',
       items: [
         'welcome/introduction',
-        'welcome/why',
         'welcome/design-principles',
         'welcome/contributing'
       ],
