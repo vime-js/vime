7cf57979463da93ebecb281c25ae0c7cfbb9b2a8
Rahim Alwer <rahim_alwer@hotmail.com>
2020-08-24 14:35:25 +1000

    feat(core): export player event enum

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,12 +1,14 @@
 import { PlayerProp, PlayerProps } from './components/core/player/PlayerProp';
 import { PlayerDispatcher, createPlayerDispatcher } from './components/core/player/PlayerDispatcher';
 import { withPlayerContext, withCustomPlayerContext } from './components/core/player/PlayerContext';
+import { PlayerEvent } from './components/core/player/PlayerEvent';
 
 export * from './components';
 
 export {
   PlayerProp,
   PlayerProps,
+  PlayerEvent,
   withPlayerContext,
   withCustomPlayerContext,
   PlayerDispatcher,
