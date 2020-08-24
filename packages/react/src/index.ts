e09eba664b0c2801a3318236836225ba70d5fc7e
Rahim Alwer <rahim_alwer@hotmail.com>
2020-08-24 14:38:24 +1000

    feat(react): export player event enum

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,12 +1,13 @@
 import { defineCustomElements } from '@vime/core/loader';
 import { usePlayerContext, usePlayerDispatcher } from './context';
-import { PlayerProp, PlayerProps, PlayerDispatcher } from '@vime/core';
+import { PlayerProp, PlayerEvent, PlayerProps, PlayerDispatcher } from '@vime/core';
 
 export * from './components';
 
 export { 
   PlayerProp,
   PlayerProps,
+  PlayerEvent,
   PlayerDispatcher,
   usePlayerContext,
   usePlayerDispatcher,
