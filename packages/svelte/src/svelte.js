6edd51b43f0c5a4bbe7ca305b78237a082b96453
Rahim Alwer <rahim_alwer@hotmail.com>
2020-09-07 14:38:08 +1000

    fix(svelte): should import player store from dist

--- packages/svelte/src/svelte.js
+++ packages/svelte/src/svelte.js
@@ -1,6 +1,6 @@
-/* eslint-disable no-undef, import/no-unresolved */
+/* eslint-disable no-undef, import/no-unresolved, import/extensions */
 import { defineCustomElements } from '@vime/core/loader';
-import { usePlayer, usePlayerStore } from '@vime/core';
+import { usePlayer, usePlayerStore } from '../dist/esm/index';
 
 export * from './svelte/index';
 export * from '@vime/core';
