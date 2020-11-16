64c76a76e43a46045da9a71133f1bee3bb34ca50
Rahim Alwer <rahim_alwer@hotmail.com>
2020-11-17 10:16:03 +1100

    fix(react): cache and event handler is shared by multiple instances

--- packages/react/src/lib.tsx
+++ packages/react/src/lib.tsx
@@ -1,6 +1,6 @@
 import composeRefs from '@seznam/compose-react-refs';
 import {
-  createElement, forwardRef, useCallback, useEffect, useState,
+  createElement, forwardRef, useCallback, useEffect, useRef, useState,
 } from 'react';
 
 export const define = (tagName: string, clazz: any) => {
@@ -14,24 +14,9 @@ const dashToPascalCase = (str: string) => str
   .map((segment) => segment.charAt(0).toUpperCase() + segment.slice(1))
   .join('');
 
-export function createComponent<T, P = {}>(tagName: string) {
-  const cache = new Map();
-  const eventHandlers = new Map();
+export function createComponent<T extends HTMLElement, P = {}>(tagName: string) {
   const isEvent = (prop: string) => prop.indexOf('on') === 0 && prop[2] === prop[2].toUpperCase();
   const toDomEventName = (prop: string) => prop[0].toLowerCase() + prop.substring(1);
-
-  function listen(ref: any, prop: string, handler?: () => void) {
-    const domEvent = toDomEventName(prop);
-    eventHandlers.get(domEvent)?.();
-    ref.addEventListener(domEvent, handler);
-    eventHandlers.set(domEvent, () => { ref.removeEventListener(domEvent, handler); });
-  }
-
-  function cleanup() {
-    cache.clear();
-    eventHandlers.forEach((fn) => fn());
-    eventHandlers.clear();
-  }
 
   const Component = forwardRef<T, P>(({
     // eslint-disable-next-line react/prop-types
@@ -40,6 +25,22 @@ export function createComponent<T, P = {}>(tagName: string) {
   }, forwardedRef) => {
     const [ref, setRef] = useState<T | null>(null);
     const setRefCb = useCallback((node: T | null) => { setRef(node); }, []);
+    const cache = useRef(new Map());
+    const eventHandlers = useRef(new Map());
+
+    const listen = useCallback((prop: string, handler?: () => void) => {
+      const domEvent = toDomEventName(prop);
+      eventHandlers.current.get(domEvent)?.();
+      if (!ref || !handler) return;
+      ref!.addEventListener(domEvent, handler);
+      eventHandlers.current.set(domEvent, () => { ref!.removeEventListener(domEvent, handler); });
+    }, []);
+
+    const cleanup = useCallback(() => {
+      cache.current.clear();
+      eventHandlers.current.forEach((fn) => fn());
+      eventHandlers.current.clear();
+    }, []);
 
     useEffect(() => () => { cleanup(); }, []);
 
@@ -50,12 +51,12 @@ export function createComponent<T, P = {}>(tagName: string) {
       }
 
       Object.keys(props).forEach((prop) => {
-        if (cache.get(prop) !== (props as any)[prop]) {
+        if (cache.current.get(prop) !== (props as any)[prop]) {
           isEvent(prop)
-            ? listen(ref, prop, (props as any)[prop])
+            ? listen(prop, (props as any)[prop])
             : ((ref as any)[prop] = (props as any)[prop]);
 
-          cache.set(prop, (props as any)[prop]);
+          cache.current.set(prop, (props as any)[prop]);
         }
       });
     }, [ref, props]);
