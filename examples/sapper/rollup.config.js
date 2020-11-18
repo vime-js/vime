8ce0019025638fd2a63a8752867d35c0da952da0
Hemanth Bollamreddi <blmhemu@gmail.com>
2020-11-18 14:18:41 +0530

    Added sapper

---
+++ examples/sapper/rollup.config.js
@@ -0,0 +1,124 @@
+import path from 'path';
+import resolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import commonjs from '@rollup/plugin-commonjs';
+import url from '@rollup/plugin-url';
+import svelte from 'rollup-plugin-svelte';
+import babel from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+import sveltePreprocess from 'svelte-preprocess';
+import typescript from '@rollup/plugin-typescript';
+import config from 'sapper/config/rollup.js';
+import pkg from './package.json';
+
+const mode = process.env.NODE_ENV;
+const dev = mode === 'development';
+const legacy = !!process.env.SAPPER_LEGACY_BUILD;
+
+const onwarn = (warning, onwarn) =>
+	(warning.code === 'MISSING_EXPORT' && /'preload'/.test(warning.message)) ||
+	(warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) ||
+	(warning.code === 'THIS_IS_UNDEFINED') ||
+	onwarn(warning);
+
+export default {
+	client: {
+		input: config.client.input().replace(/\.js$/, '.ts'),
+		output: config.client.output(),
+		plugins: [
+			replace({
+				'process.browser': true,
+				'process.env.NODE_ENV': JSON.stringify(mode)
+			}),
+			svelte({
+				dev,
+				hydratable: true,
+				preprocess: sveltePreprocess(),
+				emitCss: true
+			}),
+			url({
+				sourceDir: path.resolve(__dirname, 'src/node_modules/images'),
+				publicPath: '/client/'
+			}),
+			resolve({
+				browser: true,
+				dedupe: ['svelte']
+			}),
+			commonjs(),
+			typescript({ sourceMap: dev }),
+
+			legacy && babel({
+				extensions: ['.js', '.mjs', '.html', '.svelte'],
+				babelHelpers: 'runtime',
+				exclude: ['node_modules/@babel/**'],
+				presets: [
+					['@babel/preset-env', {
+						targets: '> 0.25%, not dead'
+					}]
+				],
+				plugins: [
+					'@babel/plugin-syntax-dynamic-import',
+					['@babel/plugin-transform-runtime', {
+						useESModules: true
+					}]
+				]
+			}),
+
+			!dev && terser({
+				module: true
+			})
+		],
+
+		preserveEntrySignatures: false,
+		onwarn,
+	},
+
+	server: {
+		input: { server: config.server.input().server.replace(/\.js$/, ".ts") },
+		output: config.server.output(),
+		plugins: [
+			replace({
+				'process.browser': false,
+				'process.env.NODE_ENV': JSON.stringify(mode)
+			}),
+			svelte({
+				generate: 'ssr',
+				hydratable: true,
+				preprocess: sveltePreprocess(),
+				dev
+			}),
+			url({
+				sourceDir: path.resolve(__dirname, 'src/node_modules/images'),
+				publicPath: '/client/',
+				emitFiles: false // already emitted by client build
+			}),
+			resolve({
+				dedupe: ['svelte']
+			}),
+			commonjs(),
+			typescript({ sourceMap: dev })
+		],
+		external: Object.keys(pkg.dependencies).concat(require('module').builtinModules),
+
+		preserveEntrySignatures: 'strict',
+		onwarn,
+	},
+
+	serviceworker: {
+		input: config.serviceworker.input().replace(/\.js$/, '.ts'),
+		output: config.serviceworker.output(),
+		plugins: [
+			resolve(),
+			replace({
+				'process.browser': true,
+				'process.env.NODE_ENV': JSON.stringify(mode)
+			}),
+			commonjs(),
+			typescript({ sourceMap: dev }),
+			!dev && terser()
+		],
+
+		preserveEntrySignatures: false,
+		onwarn,
+	}
+};
---
+++ examples/sapper/src/routes/index.svelte
@@ -0,0 +1,77 @@
+<script>
+	import {onMount} from 'svelte';
+	let vimePlayer;
+	let vimeVideo;
+	let vimeDefaultUi;
+
+	onMount(async () => {
+        const module = await import('@vime/svelte');
+		vimePlayer = module.VimePlayer;
+		vimeVideo = module.VimeVideo;
+		vimeDefaultUi = module.VimeDefaultUi;
+    });
+</script>
+
+<style>
+	h1, figure, p {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Sapper Vimejs project template</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@vime/core@^4/themes/default.css"/>
+</svelte:head>
+
+<h1>Great success!</h1>
+
+<!-- Notice we turned off controls? We're supplying our own, so we hide the native ones. -->
+<svelte:component this={vimePlayer}>
+  <svelte:component this={vimeVideo} crossOrigin="" poster="https://media.vimejs.com/poster.png">
+    <source 
+      data-src="https://media.vimejs.com/720p.mp4" 
+      type="video/mp4" 
+    />
+    <track 
+      default 
+      kind="subtitles" 
+      src="https://media.vimejs.com/subs/english.vtt" 
+      srclang="en" 
+      label="English" 
+    />
+  </svelte:component> 
+
+  <!-- We've replaced the `<VimeUi />` component. -->
+  <!-- We can turn off any features we don't want via properties. -->
+  <svelte:component this={vimeDefaultUi} noClickToPlay>
+    <!-- We can place our own UI components here to extend the default UI. -->
+  </svelte:component>
+</svelte:component>
