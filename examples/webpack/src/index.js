90395147d8d8b108a95db851ff2322dc7016e9f2
Chris Oliver <excid3@gmail.com>
2020-11-20 17:21:19 -0600

    docs(contrib): add simpler webpack example

--- examples/webpack/src/index.js
+++ examples/webpack/src/index.js
@@ -4,8 +4,13 @@ import '@vime/core/themes/default.css';
 // Optional light theme (extends default). ~400B
 import '@vime/core/themes/light.css';
 
-import { VimePlayer, VimeVideo, VimeFile } from '@vime/core';
+// Load Vime and register custom elements
+import * as vime from '@vime/core';
+vime.defineCustomElements()
 
-customElements.define('vime-player', VimePlayer);
-customElements.define('vime-video', VimeVideo);
-customElements.define('vime-file', VimeFile);
+// Or import components individually
+// import { VimePlayer, VimeVideo, VimeFile } from '@vime/core';
+
+// customElements.define('vime-player', VimePlayer);
+// customElements.define('vime-video', VimeVideo);
+// customElements.define('vime-file', VimeFile);
